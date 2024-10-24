346e1ed3f01608c336a0d48435f5f9e2beb28b26
Kirill <razdor.2019@mail.ru>
2024-10-24 21:15:19 +0300

    feat(header): create the first version of the header

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import 'reset-css';
 import './styles/styles.scss';
-
 import { Header } from './components/Header/Header.component';
+import { log } from 'console';
 
 const app = document.getElementById('app');
 
