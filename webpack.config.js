81284c630bd455f8647e57e4ecec9e6471661abe
Kirill <razdor.2019@mail.ru>
2024-10-24 21:13:58 +0300

    chore: configuring the config, add icons and logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,60 @@
-const { rules } = require('eslint-config-prettier');
-const { default: test } = require('node:test');
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.html', '.scss'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+    entry: './src/main.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
-    open: true,
-    port: 3000,
-  },
-};
+    mode: 'development',
+    resolve: {
+        extensions: ['.ts', '.js', '.scss', '.css', '.html'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+
+            {
+                test: /\.module\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]__[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /\.module\.s[ac]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        port: 3000,
+    },
+}
