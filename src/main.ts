f5365116c35b57454af6d2b1699589398cc53ed9
Kirill <razdor.2019@mail.ru>
2024-10-25 14:39:50 +0300

    chore: fix config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import 'reset-css';
-import './styles/styles.scss';
-import { Header } from './components/Header/Header.component';
-import { log } from 'console';
+import 'reset-css'
+import './styles/styles.scss'
+import { Header } from './components/Header/Header.component'
 
-const app = document.getElementById('app');
+import { Main } from './components/Main/Main.component'
+import { Banner } from './components/Slider/Slider.component'
+
+const app = document.getElementById('app')
 
 if (app) {
-  const header = new Header();
-
-  header.appendTo(app);
+    const header = new Header()
+    const main = new Main()
+    const banner = new Banner()
+    header.appendTo(app)
+    main.appendTo(app)
+    banner.appendTo(main.element)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { default: test } = require('node:test')
+const { type } = require('os')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
     entry: './src/main.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        publicPath: '/', // Путь к статическим ресурсам
     },
     mode: 'development',
     resolve: {
@@ -17,7 +21,6 @@ module.exports = {
                 test: /\.html$/,
                 use: 'html-loader',
             },
-
             {
                 test: /\.module\.s[ac]ss$/i,
                 use: [
@@ -33,28 +36,47 @@ module.exports = {
                     'sass-loader',
                 ],
             },
-
             {
                 test: /\.s[ac]ss$/i,
                 exclude: /\.module\.s[ac]ss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
-
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader'], // Обработка CSS файлов
             },
-
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource', // используем asset/resource для Webpack 5
+                generator: {
+                    filename: 'assets/images/[name][hash][ext]', // куда сохранять
+                },
+            },
         ],
     },
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist',
+        static: {
+            directory: path.join(__dirname, 'public'), // Папка для статических файлов
+        },
         port: 3000,
+        hot: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // укажите путь к вашему шаблону
+            filename: 'index.html',
+            inject: true, // автоматически вставляет ссылки на сгенерированные скрипты
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/assets', to: 'assets' }, // копируем папку assets из public в dist
+            ],
+        }),
+    ],
 }
