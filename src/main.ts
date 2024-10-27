326546f69a8d10f95c06a36e0b0e57bfaed5c3d1
Kirill <razdor.2019@mail.ru>
2024-10-27 12:21:44 +0300

    feat: add BooksWrapper to main

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Header } from './components/Header/Header.component'
 
 import { Main } from './components/Main/Main.component'
 import { Banner } from './components/Slider/Slider.component'
+import { BooksWrapper } from './components/BooksWrapper/BooksWrapper.component'
 
 const app = document.getElementById('app')
 
@@ -11,7 +12,9 @@ if (app) {
     const header = new Header()
     const main = new Main()
     const banner = new Banner()
+    const booksWrapper = new BooksWrapper()
     header.appendTo(app)
     main.appendTo(app)
     banner.appendTo(main.element)
+    booksWrapper.appendTo(main.element)
 }
